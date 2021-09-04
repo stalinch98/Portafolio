9aae701b4c7199ef4ac84b6313541b75e601c143
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-09-04 14:58:16 -0500

    divider

---
+++ src/components/Divider.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/components/Divider.scss'
+import consola from '../assets/static/console.jpg';
+
+const Divider = () => (
+    <div className="container3" style={{ backgroundImage: `url(${consola})` }}>
+    </div>
+);
+
+export default Divider
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,6 +5,7 @@ import About from '../components/About';
 import Education from '../components/Education';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
+import Divider from '../components/Divider';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
@@ -17,6 +18,7 @@ const App = () => (
     <Education/>
     <Projects/>
     <Skills/>
+    <Divider/>
     <Contact/>
     <Footer/>
   </div>

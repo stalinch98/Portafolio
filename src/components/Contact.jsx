15dfd9dc71155820f85204ded1bc59bfac19f86b
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-09-16 00:57:14 -0500

    use api

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
+import swal from 'sweetalert';
+import axios from 'axios';
 import '../assets/styles/components/Contact.scss'
 import divider from '../assets/static/section-divider.png';
 const url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2042776.193951151!2d-79.54981257958015!3d-0.18619061570813528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59a4002427c9f%3A0x44b991e158ef5572!2sQuito!5e0!3m2!1sen!2sec!4v1629959521728!5m2!1sen!2sec";
+const baseURL = "https://portafolio-chiguano.herokuapp.com/api/send";
 const Contact = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-    
+
         const emailInput = event.target.email;
         const nameInput = event.target.name;
         const asuntoInput = event.target.asunto;
         const msjInput = event.target.msj;
-    
-        console.log(emailInput.value);
-        console.log(nameInput.value); 
-        console.log(asuntoInput.value); 
-        console.log(msjInput.value); 
+
+        const mesaje = {
+            "name": nameInput.value,
+            "from": emailInput.value,
+            "subject": asuntoInput.value,
+            "msj": msjInput.value
+        };
+        axios.post(baseURL, mesaje)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+
+        swal({
+            title: "Mensaje Enviado",
+            text: "Pronto me pondré en contacto contigo",
+            icon: "success",
+            button: "Aceptar"
+        });
+
+        nameInput.value = "";
+        emailInput.value = "";
+        asuntoInput.value = "";
+        msjInput.value = "";
     };
 
     return (
@@ -45,15 +69,15 @@ const Contact = () => {
                             <h3>ENVÍAME UN MENSAJE CORTO</h3>
                             <form onSubmit={handleSubmit} method="POST">
                                 <label htmlFor="name">Nombre</label>
-                                <input type="text" id="name" name="name"/>
+                                <input type="text" id="name" name="name" required />
 
                                 <label htmlFor="email">Correo electrónico</label>
-                                <input type="text" id="email" name="email"/>
+                                <input type="text" id="email" name="email" required />
 
                                 <label htmlFor="asunto">Asunto</label>
-                                <input type="text" id="asunto" name="asunto"/>
+                                <input type="text" id="asunto" name="asunto" required />
                                 <label htmlFor="msj">Mensaje</label>
-                                <textarea id="msj" name="msj" cols="30" rows="10"></textarea>
+                                <textarea id="msj" name="msj" cols="30" rows="10" required></textarea>
 
                                 <input type="submit" value="ENVIAR" />
                             </form>
